4cc4c7cfbbb8f30e22c5b265f250733c88cd9c5a
Ambar Tiwari <ambartiwariji1995@gmail.com>
2025-06-27 10:02:44 +0530

    Day09:Add program to demonstrate Python variable types and type() usage

---
+++ Day09/all_data_types_example.py
@@ -0,0 +1,19 @@
+# Variable Definitions
+name = "Ambar"                     # str
+age = 22                           # int
+height = 5.9                       # float
+is_student = True                 # bool
+colors = ["red", "blue", "green"] # list
+coordinates = (10, 20)            # tuple
+user = {"name": "Ambar", "age": 22}  # dict
+unique_numbers = {1, 2, 3}        # set
+
+# Print value and type of each variable
+print("name:", name, "| type:", type(name))
+print("age:", age, "| type:", type(age))
+print("height:", height, "| type:", type(height))
+print("is_student:", is_student, "| type:", type(is_student))
+print("colors:", colors, "| type:", type(colors))
+print("coordinates:", coordinates, "| type:", type(coordinates))
+print("user:", user, "| type:", type(user))
+print("unique_numbers:", unique_numbers, "| type:", type(unique_numbers))
